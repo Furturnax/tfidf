e4cd478572ce8069a77a71a4dde97ba2be169fe5
Andrew Fedorchenko <furturnax@gmail.com>
2024-04-04 15:44:23 +0300

    Update views

--- loader/views.py
+++ loader/views.py
@@ -1,38 +1,43 @@
-from django.shortcuts import render
-from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from collections import Counter
+from math import log
 
+from django.shortcuts import render
+
+from .consts import LIMIT_WORDS
 from .models import Word
 
 
-def text_process(file):
-    """Обработка текста."""
-    text = file.read().decode('utf-8')
+def process_file(text):
+    """Обработка документа с подсчетом TF и IDF."""
+    words_table = []
+    words = re.split(r'\W+', text)
+    word_counter = Counter(words)
+    for word, tf in word_counter.items():
+        words_table.append(
+            Word(
+                word=word.capitalize(),
+                tf=tf,
+                idf=log(len(words) / (1 + word_counter[word]))
+            )
+        )
+    return Word.objects.bulk_create(words_table)
 
-    vectorizer = TfidfVectorizer()
-    words = vectorizer.get_feature_names()
-    ftidf_matrix = vectorizer.fit_transform([text])
-    tfidf_values = ftidf_matrix.toarray()[0]
 
-    data = [
-        (
-            word, tfidf_values[i], vectorizer.idf_[i]
-        ) for i, word in enumerate(words)
-    ]
-
-    return data
+def render_results(request):
+    """Рендер страницы с результатами подсчета TF и IDF."""
+    return render(
+        request,
+        'results.html',
+        {'words': Word.objects.order_by('-idf')[:LIMIT_WORDS]}
+    )
 
 
 def file_upload(request):
-    """Загрузка файла."""
-    if request.method == 'POST' and request.FILES['file']:
-        data = text_process(request.FILES['file'])
-
-        for word, tf, idf in data:
-            Word.objects.create(word=word, tf=tf, idf=idf)
-
-        return render(
-            request,
-            'results.html',
-            {'data': Word.objects.order_by('-idf')[:50]}
-        )
-    return render(request, 'upload.html')
+    """Загрузка документа с последующим подсчетом TF и IDF."""
+    Word.objects.all().delete()
+    if not (request.method == 'POST' and request.FILES.get('file')):
+        return render(request, 'upload.html')
+    text = request.FILES['file'].read().decode('utf-8')
+    process_file(text)
+    return render_results(request)
